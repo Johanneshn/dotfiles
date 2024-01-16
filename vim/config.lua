d1d7cf63e5e11f1a77dc68e4880bc74a1a4fc5d2
Johanneshn <jhn@kth.se>
2024-01-16 21:54:18 +0100

    Update config.lua

--- vim/config.lua
+++ vim/config.lua
@@ -30,6 +30,26 @@ vim.g.clipboard = {
 lvim.builtin.autopairs.active = false
 -- plugins
 lvim.plugins = {
+  { "folke/todo-comments.nvim" },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,       -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,             -- Function to run after the scrolling animation ends
+      })
+    end
+  },
   {
     "mg979/vim-visual-multi"
   },
@@ -130,7 +150,17 @@ end
 
 
 -- Trouble 
-vim.keymap.set("n", "<leader>xw", function() require("trouble").open() end)
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Diagnostics",
+  t = { "<cmd>TroubleToggle<cr>", "trouble" },
+  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+}
+
+
 
 vim.keymap.set('n', ',W', swap_windows, { desc = 'Swap windows' })
 vim.keymap.set('i', 'jk', '<Esc>')
