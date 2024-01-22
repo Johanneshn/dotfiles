2af1c26ae02bbdb61363171c20913ff06751669d
Johannes <jhn@kth.se>
2024-01-22 21:01:58 +0100

    Update config.lua

--- vim/config.lua
+++ vim/config.lua
@@ -1,4 +1,4 @@
---  Read the docs: https://www.lunarvim.org/docs/configuration
+-- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/
@@ -26,29 +26,34 @@ vim.g.clipboard = {
 }
 
 
-
-lvim.builtin.autopairs.active = false
+lvim.builtin.autopairs.active = true
 -- plugins
 lvim.plugins = {
-  { "folke/todo-comments.nvim" },
+  { 'tpope/vim-surround' },
   {
-    "karb94/neoscroll.nvim",
-    event = "WinScrolled",
-    config = function()
-      require('neoscroll').setup({
-        -- All these keys will be mapped to their corresponding default scrolling animation
-        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
-          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-        hide_cursor = true,          -- Hide cursor while scrolling
-        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = nil,       -- Default easing function
-        pre_hook = nil,              -- Function to run before the scrolling animation starts
-        post_hook = nil,             -- Function to run after the scrolling animation ends
-      })
-    end
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    opts = {
+      transparent = false,
+      theme = "wave",
+      background = {
+        -- dark = "dragon"
+      },
+      colors = {
+        theme = {
+          -- wave = {
+          --   ui = {
+          --     bg = '#181616'
+          --   }
+          -- },
+          all = {
+            ui = {
+              bg_gutter = "none"
+            }
+          }
+        }
+      }
+    }
   },
   {
     "mg979/vim-visual-multi"
@@ -59,54 +64,28 @@ lvim.plugins = {
     config = function()
       require("hop").setup()
       vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent =true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
     end,
   },
-  -- {
-  --   "m4xshen/hardtime.nvim",
-  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  --   opts = {}
-  -- },
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
   },
-  {
-    "zbirenbaum/copilot-cmp",
-    event = "InsertEnter",
-    dependencies = { "zbirenbaum/copilot.lua" },
-    config = function()
-      vim.defer_fn(function()
-        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-      end, 100)
-    end,
-  },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   event = "InsertEnter",
+  --   dependencies = { "zbirenbaum/copilot.lua" },
+  --   config = function()
+  --     vim.defer_fn(function()
+  --       require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+  --       require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+  --     end, 100)
+  --   end,
+  -- },
   {
     "sindrets/diffview.nvim",
     event = "BufRead",
-  },
-  {
-    "s1n7ax/nvim-window-picker",
-    version = "1.*",
-    config = function()
-      require("window-picker").setup({
-        autoselect_one = true,
-        include_current = false,
-        filter_rules = {
-          -- filter using buffer options
-          bo = {
-            -- if the file type is one of following, the window will be ignored
-            filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
-
-            -- if the buffer type is one of following, the window will be ignored
-            buftype = { "terminal" },
-          },
-        },
-        other_win_hl_color = "#e35e4f",
-      })
-    end,
   },
   {
     "folke/which-key.nvim",
@@ -120,58 +99,27 @@ lvim.plugins = {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     }
-  }, 
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
 
-lvim.colorscheme = "catppuccin-macchiato"
-
+-- lvim.colorscheme = "catppuccin-macchiato"
+lvim.colorscheme = 'kanagawa-wave'
 -- Remove the CTRL-\ for terminal and restore c-t
 lvim.builtin.terminal.open_mapping = "<c-t>"
 -- Enable use of C-l to clear the terminal
 lvim.keys.term_mode = { ["<C-l>"] = false }
 
 
--- example mappings you can place in some other place
--- An awesome method to jump to windows
-local picker = require('window-picker')
-
--- Swap two windows using the awesome window picker
-local function swap_windows()
-  local window = picker.pick_window({
-    include_current_win = false
-  })
-  local target_buffer = vim.fn.winbufnr(window)
-  -- Set the target window to contain current buffer
-  vim.api.nvim_win_set_buf(window, 0)
-  -- Set current window to contain target buffer
-  vim.api.nvim_win_set_buf(0, target_buffer)
-end
-
-
--- Trouble 
-lvim.builtin.which_key.mappings["t"] = {
-  name = "Diagnostics",
-  t = { "<cmd>TroubleToggle<cr>", "trouble" },
-  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
-  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
-  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
-  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
-}
-
-
-
-vim.keymap.set('n', ',W', swap_windows, { desc = 'Swap windows' })
+-- Trouble
+vim.keymap.set("n", "<leader>xw", function() require("trouble").open() end)
 vim.keymap.set('i', 'jk', '<Esc>')
-vim.opt.relativenumber = true -- relative line numbers
-vim.keymap.set("n", ",w", function()
-  local picked_window_id = picker.pick_window({
-    include_current_win = true,
-    hint = 'floating-big-letter'
-  }) or vim.api.nvim_get_current_win()
-  vim.api.nvim_set_current_win(picked_window_id)
-end, { desc = "Pick a window" })
 
 vim.opt.relativenumber = true -- relative line numbers
-lvim.transparent_window = true
+lvim.transparent_window = false
+
+
+vim.keymap.set('n', 'H', '^')
+vim.keymap.set('n', 'J', '}')
+vim.keymap.set('n', 'K', '{')
+vim.keymap.set('n', 'L', '$')
