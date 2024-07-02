a8faa3b7d362bff7e7be243d0fe5dae5e6627ac2
Johannes <jhn@kth.se>
2024-07-02 21:47:46 +0200

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,34 +2,22 @@
 local wezterm = require("wezterm")
 local tab = require("tab")
 local fonts = require("fonts")
-local keys = require("keys") -- local kanagawa = require("themes.kanagawa")
+local keys = require("keys")
 local config = {}
--- Pywal config
--- wezterm.add_to_config_reload_watch_list("home/emretuna/.wezterm.lua")
--- wezterm.add_to_config_reload_watch_list("home/emretuna/.cache/wal/wezterm-wal.toml")
--- config.color_scheme_dirs = { "/home/emretuna/.cache/wal" }
--- config.color_scheme = "Pywal"
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
--- This module should set the appearance-related options.
--- Configurations
---
--- Set the Kanagawa theme as the default
 config.color_scheme = "Kanagawa (Gogh)"
--- config.color_scheme = "tokyonight_night"
 config.term = "wezterm"
 config.warn_about_missing_glyphs = true
 config.enable_scroll_bar = false
--- config.window_padding = {
--- 	left = "0.5cell",
--- 	right = "0cell",
--- 	top = "1.50cell",
--- 	bottom = "0cell",
--- }
+config.window_padding = {
+	left = "0.5cell",
+	right = "0cell",
+	top = "1.50cell",
+	bottom = "0cell",
+}
 config.scrollback_lines = 10000
 config.window_decorations = "INTEGRATED_BUTTONS"
 
@@ -51,7 +39,7 @@ config.default_cursor_style = "BlinkingBlock"
 config.cursor_blink_rate = 800
 
 config.default_prog = { "pwsh.exe", "-NoLogo" }
--- and finally, return the configuration to wezter
+
 tab.setup(config)
 fonts.setup(config)
 keys.setup(config)
--- wezterm/wezterm/keys.lua
+++ wezterm/wezterm/keys.lua
@@ -1,13 +1,9 @@
 local wezterm = require("wezterm")
 local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 local Keys = {}
--- you can put the rest of your Wezterm config here
+
 function Keys.setup(config)
 	smart_splits.apply_to_config(config, {
-		-- the default config is here, if you'd like to use the default keys,
-		-- you can omit this configuration table parameter and just use
-		-- smart_splits.apply_to_config(config)
-
 		-- directional keys to use in order of: left, down, up, right
 		direction_keys = { "h", "j", "k", "l" },
 		-- modifier keys to combine with direction_keys
@@ -30,7 +26,11 @@ function Keys.setup(config)
 	config.keys = {
 		{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
 		{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-		{ key = "\\", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+		{
+			key = "\\",
+			mods = "LEADER",
+			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+		},
 		{ key = "s", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 		{ key = "v", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
 		{ key = "o", mods = "LEADER", action = "TogglePaneZoomState" },
@@ -57,107 +57,6 @@ function Keys.setup(config)
 		{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 		{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 	}
-	-- config.keys = {
-	-- 	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
-	-- 	{
-	-- 		key = "l",
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.ActivatePaneDirection("Right"),
-	-- 	},
-	-- 	{
-	-- 		key = "k",
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.ActivatePaneDirection("Up"),
-	-- 	},
-	-- 	{
-	-- 		key = "j",
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.ActivatePaneDirection("Down"),
-	-- 	},
-	-- 	{
-	-- 		key = "LeftArrow",
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-	-- 	},
-	-- 	{
-	-- 		key = "DownArrow",
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-	-- 	},
-	-- 	{
-	-- 		key = "UpArrow",
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-	-- 	},
-	-- 	{
-	-- 		key = "RightArrow",
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-	-- 	},
-	-- 	{
-	-- 		key = [[\]],
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action({
-	-- 			SplitHorizontal = { domain = "CurrentPaneDomain" },
-	-- 		}),
-	-- 	},
-	-- 	{
-	-- 		key = [[|]],
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.SplitPane({
-	-- 			top_level = true,
-	-- 			direction = "Right",
-	-- 			size = { Percent = 50 },
-	-- 		}),
-	-- 	},
-	-- 	{
-	-- 		key = [[-]],
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action({
-	-- 			SplitVertical = { domain = "CurrentPaneDomain" },
-	-- 		}),
-	-- 	},
-	-- 	{
-	-- 		key = [[_]],
-	-- 		mods = "LEADER",
-	-- 		action = wezterm.action.SplitPane({
-	-- 			top_level = true,
-	-- 			direction = "Down",
-	-- 			size = { Percent = 50 },
-	-- 		}),
-	-- 	},
-	-- 	{
-	-- 		key = "n",
-	-- 		mods = "ALT",
-	-- 		action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
-	-- 	},
-	-- 	{
-	-- 		key = "Q",
-	-- 		mods = "ALT",
-	-- 		action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
-	-- 	},
-	-- 	{ key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-	-- 	{ key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
-	-- 	{ key = "[", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-	-- 	{ key = "]", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
-	-- 	{ key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
-	-- 	{ key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
-	-- 	{ key = "y", mods = "ALT", action = wezterm.action.ActivateCopyMode },
-	-- 	{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
-	-- 	{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-	-- 	{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-	-- 	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-	-- 	{ key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
-	-- 	{ key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
-	-- 	{ key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
-	-- 	{ key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
-	-- 	{ key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
-	-- 	{ key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
-	-- 	{ key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
-	-- 	{ key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
-	-- 	{ key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
-	-- }
 end
 
--- return keys and mouse
 return Keys
