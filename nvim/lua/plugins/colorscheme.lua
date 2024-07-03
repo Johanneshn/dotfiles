7016b164f355eb0e96d5acbbb228e0c59c3b7b29
Johannes <jhn@kth.se>
2024-07-03 20:57:36 +0200

    add nvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--     },
+--   },
+-- }
+--
+return {
+  { "rebelot/kanagawa.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
