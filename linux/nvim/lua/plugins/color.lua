0ff63b6f3f81894806f68b8bb2f7434b8e9f4e79
Johannes <jhn@kth.se>
2025-02-06 20:22:41 +0100

    update linux files

--- linux/nvim/lua/plugins/color.lua
+++ linux/nvim/lua/plugins/color.lua
@@ -1,24 +1,11 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  enabled = true,
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      transparent_background = true,
-      custom_highlights = function(colors)
-        return {
-          Folded = { bg = colors.none },
-          UfoFoldedEllipsis = { fg = colors.blue, bg = colors.none },
-        }
-      end,
-      integrations = {
-        notify = true,
-      }
-    })
-
-    vim.cmd.colorscheme("catppuccin")
-  end
+	{ "Mofiqul/vscode.nvim", options = {
+		transparent = true,
+	} },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "vscode",
+		},
+	},
 }
